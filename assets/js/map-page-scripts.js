8e5edae3e86e32b89a3c04e992fd8200f1d5a53d
mattingliscoding <mattingliscoding@gmail.com>
2020-09-12 12:42:35 +0000

    Footer finalised & responsive breakpoints underway for prominent secionts of the site

--- assets/js/map-page-scripts.js
+++ assets/js/map-page-scripts.js
@@ -1,137 +1,112 @@
-$(document).ready(function() {
-        initialize(); // init map
-      });
+var restaurants = "assets/data/restaurants.json";
+var hotels = "assets/data/hotels.json";
+var thingsToDo = "assets/data/thingsToDo.json";
+
+// This is in Bielefeld
+const initialPosition = {
+    'lat': 52.022,
+    'lng': 8.532
+}
+const zoomLevel = 13.5;
+var infowindow;
+var openMarker;
+
+var restaurantMarkers = [];
+var hotelMarkers = [];
+var thingsToDoMarkers = [];
+
+
+$(document).ready(function () {
+    initMap(); // init map
+    loadMarkers(thingsToDo, "assets/images/icons/thingsToDoIcon.png", thingsToDoDisplayFunction);
+    loadMarkers(restaurants, "assets/images/icons/restaurantIcon.png", restaurantDisplayFunction);
+    loadMarkers(hotels, "assets/images/icons/hotelIcon.png", hotelDisplayFunction);
+});
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("large-map"), {
-    center: {
-      lat: 52.022,
-      lng: 8.532,
-    },
-    zoom: 13.5,
-    mapTypeId: "hybrid",
-    
-  });
+    map = new google.maps.Map(document.getElementById("large-map"), {
+        center: {
+            lat: initialPosition.lat,
+            lng: initialPosition.lng,
+        },
+        zoom: zoomLevel,
+        mapTypeId: "hybrid",
+
+    });
 }
 
-const restaurants = "assets/data/restaurants.json";
-const hotels = "assets/data/hotels.json";
-const thingsToDo = "assets/data/thingsToDo.json"
-let infowindow;
-
-$.getJSON(restaurants, function (data) { 
-  // Loop through the data
-  $.each(data, function (key, data) {
-    let myLatlng = new google.maps.LatLng(data.lat, data.lng); // set position
-
-    // Add marker to map
-    let restaurantIcon = "assets/images/icons/restaurantIcon.png";
-    
-    let marker = new google.maps.Marker({
-      position: myLatlng,
-      icon: restaurantIcon,
-      animation: google.maps.Animation.DROP,
-      map: map,
+function addMarkerListener(infoContent, marker) {
+    marker.info = new google.maps.InfoWindow({
+        content: infoContent,
+        maxWidth: 200
     });
 
+    google.maps.event.addListener(marker, "click", function () {
+        if (openMarker != null) {
+            openMarker.info.close();
+        }
+        marker.info.open(map, marker);
+        openMarker = marker;
+    });
+
+}
+
+console.log(restaurantMarkers);
+
+function restaurantDisplayFunction(data) {
     // Set info window content
-    let infoContent = "<h4>" + data.restaurantName + "</h4>";
+    infoContent = "<h4>" + data.restaurantName + "</h4>";
     infoContent += "<p>" + data.foodType + "</p>";
     infoContent += "<p>" + data.rating + "</p>";
     infoContent += "<p>" + "'" + data.reviews1 + "'" + "</g>";
     infoContent += "<p>" + "'" + data.reviews2 + "'" + "</p>";
-    
-    // Add listener for info window
-    google.maps.event.addListener(marker, "click", function () {
-        if (marker.info != null) {
-            marker.info.close() // close any other infowindows first
-        }
-        marker.info = new google.maps.InfoWindow({
-        content: infoContent, maxWidth: 200 
-    });
-        marker.info.open(map, marker);
-    });
-   
-  });
-});
+    return infoContent;
+}
 
-$.getJSON(hotels, function (data) {
-  // Loop through the data
-  $.each(data, function (key, data) {
-    let myLatlng = new google.maps.LatLng(data.lat, data.lng); // set position
-
-    // Add marker to map
-    let hotelIcon = "assets/images/icons/hotelIcon.png";
-    
-    let marker = new google.maps.Marker({
-      position: myLatlng,
-      icon: hotelIcon,
-      animation: google.maps.Animation.DROP,
-      map: map,
-    });
-
+function hotelDisplayFunction(data) {
     // Set info window content
-    let infoContent = "<h4>" + data.hotelName + "</h4>";
-      infoContent += "<p>" + data.customerRating + "</p>";
-      infoContent += "<p>" + data.feature1 + "</g>";
-      infoContent += "<p>" + data.feature2 + "</p>";
-    
+    infoContent = "<h4>" + data.hotelName + "</h4>";
+    infoContent += "<p>" + data.customerRating + "</p>";
+    infoContent += "<p>" + data.feature1 + "</g>";
+    infoContent += "<p>" + data.feature2 + "</p>";
+    return infoContent;
+}
 
-    // Add info window
-    if (infowindow != null) {
-        infowindow.close() // close any other infowindows first
-    }
-    
-    marker.info = new google.maps.InfoWindow({
-      content: infoContent, maxWidth: 200 
-    });
-
-    // Add listener for info window
-    google.maps.event.addListener(marker, "click", function () {
-      marker.info.open(map, marker);
-    });
-
-
-   
-  });
-});
-
-$.getJSON(thingsToDo, function (data) {
-  // Loop through the data
-  $.each(data, function (key, data) {
-    let myLatlng = new google.maps.LatLng(data.lat, data.lng); // set position
-
-    // Add marker to map
-    let thingsToDoIcon = "assets/images/icons/thingsToDoIcon.png";
-    
-    let marker = new google.maps.Marker({
-      position: myLatlng,
-      icon: thingsToDoIcon,
-      animation: google.maps.Animation.DROP,
-      map: map,
-    });
-
+function thingsToDoDisplayFunction(data) {
     // Set info window content
-    let infoContent = "<h4>" + data.activityName + "</h4>";
-      infoContent += "<p>" + data.activityRating + "</p>";
-      infoContent += "<p>" + data.activityInfo + "</g>";
-      
+    infoContent = "<h4>" + data.activityName + "</h4>";
+    infoContent += "<p>" + data.activityRating + "</p>";
+    infoContent += "<p>" + data.activityInfo + "</g>";
+    return infoContent;
+}
 
-    // Add info window
-    if (infowindow != null) {
-        infowindow.close() // close any other infowindows first
-    }
-    
-    marker.info = new google.maps.InfoWindow({
-      content: infoContent, maxWidth: 200 
+function loadMarkers(jsonData, iconPath, infoDisplayFunction) {
+    $.getJSON(jsonData, function (data) {
+        // Loop through the data
+        $.each(data, function (key, data) {
+            let myLatlng = new google.maps.LatLng(data.lat, data.lng); // set position
+            let marker = new google.maps.Marker({
+                position: myLatlng,
+                icon: iconPath,
+                animation: google.maps.Animation.DROP,
+                map: map,
+            });
+
+            let content = infoDisplayFunction(data);
+            addMarkerListener(content, marker);
+        });
     });
+}
 
-    // Add listener for info window
-    google.maps.event.addListener(marker, "click", function () {
-      marker.info.open(map, marker);
-    });
+function toggleMarker(){
+	if (document.getElementById("toggleMarkers").checked)
+		show = map;
+	else
+	    show = null;
+	for (var i = 0; i < markers.length; i++) {
+		markers[i].setMap(show);
+	}
+}
 
-   
-  });
-});
+
 
--- assets/js/sign-up-scripts.js
+++ assets/js/sign-up-scripts.js
@@ -1,34 +1,25 @@
-//  window.onload = function() {
-//             document.getElementById('contact-form').addEventListener('submit', function(event) {
-//                 event.preventDefault();
-//                 // generate the contact number value
-//                 this.contact_number.value = Math.random() * 100000 | 0;
-//                 emailjs.sendForm('gmail', 'bielefeld', '#contact-form', "user_eWrL0zPfiwLFrEZOHu8uj")
-//                     .then(function (response) {
-//                         console.log('SUCCESS!', response.status, response.text);
-//                     }, function (error) {
-//                         console.log('FAILED...', error);
-//                     });
-//             });
-//         }
-        
 const btn = document.getElementById('button');
 
 document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+    .addEventListener('submit', function (event) {
+        btn.disabled = true;
+        event.preventDefault();
 
-   btn.value = 'Sending...';
+        btn.value = 'Sending...';
 
-   const serviceID = 'default_service';
-   const templateID = 'bielefeld';
+        const serviceID = 'default_service';
+        const templateID = 'bielefeld';
 
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Send Email';
-      alert('Sent!');
-    }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.value = 'Send Email';
+                alert('Sent!');
+            }, (err) => {
+                btn.value = 'Send Email';
+                alert(JSON.stringify(err));
+            })
+            .finally(() => {
+                this.reset();
+                btn.disabled = false;
+            });
     });
-});
